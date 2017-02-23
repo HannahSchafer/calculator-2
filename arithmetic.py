a31f323be959ad07c8bbeb081ee4fb527cee628e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-23 12:59:57 -0800

    WIP power

--- arithmetic.py
+++ arithmetic.py
@@ -1,62 +1,83 @@
 """TODO
 -alter calc function
-    make list of nums (done)
-    feed list into function
+    make numbers of nums (done)
+    feed numbers into function
 -change params on functions
 -change the body the arithmetic functions
     iteration!
 """
 
 
-def add(list):
+def add(numbers):
     counter = 0
-    for item in list:
+    for item in numbers:
         counter += item
     return counter
 
 
-def subtract(list):
-    counter = list[0]
-    for item in list[1:]:
+def subtract(numbers):
+    counter = numbers[0]
+    for item in numbers[1:]:
         counter -= item
     return counter
 
 
-def multiply(list):
+def multiply(numbers):
     counter = 1
-    for item in list:
+    for item in numbers:
         counter *= item
     return counter
 
 
-def divide(list):
+def divide(numbers):
     # Need to turn at least argument to float for division to
     # not be integer division
-    counter = float(list[0])
-    for item in list[1:]:
+    counter = float(numbers[0])
+    for item in numbers[1:]:
         counter /= float(item)
     return counter
 
 
-def square(list):
+def square(numbers):
     # Needs only one argument
     counter = []
-    for item in list:
+    for item in numbers:
         counter.append(item**2)
     return counter
 
 
-def cube(list):
+def cube(numbers):
     # Needs only one argument
     counter = []
-    for item in list:
+    for item in numbers:
         counter.append(item**3)
     return counter
 
 
-def power(num1, num2):
-    return num1 ** num2  # ** = exponent operator
+def power(numbers):
+    new_expo = 1
+    while len(numbers) > 0:
+        exponent = numbers.pop()
+        new_expo =numbers[-1]**exponent
+    return new_expo
+
+
+
 
 
 def mod(num1, num2):
     return num1 % num2
+
+"""
+def power(numbers):
+    def recursor(num, numbers_2):
+        #base case
+        if len(numbers_2) == 1:
+            return num**numbers_2[0]
+        elif len(numbers_2) < 1:
+            print "You need at least 2 numbers."
+        else: 
+            new_num = numbers_2.pop(0)
+            return num ** recursor(new_num, numbers_2)
+    return recursor(numbers[0], numbers[1:])
+"""
